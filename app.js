2bb1bd05c3406662736f036e95eb1521dbcb1509
Anastasia <anastasia151000@rambler.ru>
2022-10-02 14:45:49 +0300

    work_activeTask

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const items_active = document.querySelector(".items_active");
 const items_completed = document.querySelector(".items_completed");
 const items_delete = document.querySelector(".items_delete");
 
+const tasksListElement = document.querySelector(`.tasks__list`);
+
 
 const btn_add = document.getElementById('btn__add');
 const trash = document.querySelector(".bxs_trash");
@@ -14,7 +16,7 @@ document.addEventListener('keyup', function (e) {
     }
 });
 
-btn_add.addEventListener("click", ()=> {
+btn_add.addEventListener("click", () => {
     let task = document.querySelector(".input_task").value;
     if (task == "" || task == " ") { return }
     else {
@@ -31,7 +33,8 @@ function createTask(task) {
     let bx_check = document.createElement('i');
     let bxs_trash = document.createElement('i');
     bxs_items.className = "bxs_items";
-    div.className = "alert";
+    div.className = "alert tasks__item";
+    div.draggable = true;
     div.textContent = task;
 
     bx_check.className = "bx-check bx";
@@ -133,3 +136,50 @@ function loadTasks() {
     }
     else return;
 }
+
+
+
+
+tasksListElement.addEventListener(`dragstart`, (evt) => {
+    evt.target.classList.add(`selected`);
+});
+
+tasksListElement.addEventListener(`dragend`, (evt) => {
+    evt.target.classList.remove(`selected`);
+});
+
+const getNextElement = (cursorPosition, currentElement) => {
+    const currentElementCoord = currentElement.getBoundingClientRect();
+    const currentElementCenter = currentElementCoord.y + currentElementCoord.height / 2;
+
+    const nextElement = (cursorPosition < currentElementCenter) ?
+        currentElement :
+        currentElement.nextElementSibling;
+
+    return nextElement;
+};
+
+tasksListElement.addEventListener(`dragover`, (evt) => {
+    evt.preventDefault();
+
+    const activeElement = tasksListElement.querySelector(`.selected`);
+    const currentElement = evt.target;
+    const isMoveable = activeElement !== currentElement &&
+        currentElement.classList.contains(`tasks__item`);
+
+    if (!isMoveable) {
+        return;
+    }
+
+    const nextElement = getNextElement(evt.clientY, currentElement);
+
+    if (
+        nextElement &&
+        activeElement === nextElement.previousElementSibling ||
+        activeElement === nextElement
+    ) {
+        return;
+    }
+
+    tasksListElement.insertBefore(activeElement, nextElement);
+});
